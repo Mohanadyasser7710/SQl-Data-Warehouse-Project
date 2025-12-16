405aa7d9852b86acbd3c295d88114d5a89f54307
Mohanadyasser7710 <mohanadyasser666@gmail.com>
2025-12-16 20:34:25 +0200

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,22 @@
+/*
+
+create database and schem
+=====================================
+script purpose:
+creating database named "DataWarehouse" and schemas after checking if it exists or no 
+
+
+WARNNING:
+this script will delete databse named 'DataWarehouse' if it exists
+
+*/
+
+
+
+
+
+
+
 USE master;
 
 IF EXISTS(SELECT 1 FROM sys.databases WHERE name='DataWarehouse')
